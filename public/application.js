4ef7df159b920f741af5766189403667c7de4843
Jason <jasonhoekstra@gmail.com>
2014-12-11 05:08:48 -0500

    Re-title to Author and add Time Required.

--- public/application.js
+++ public/application.js
@@ -48,10 +48,29 @@ $('#alignmentsModal').on('show', function () {
 $(document).on("click",".closetdd",function(){
 	var $thisc = $(this);
 	$thisc.parent().parent().hide("slow").remove();
-})
+});
 
 $(function() {
 	$('#new_btn').click(function() {
 		return window.confirm("Are you sure you would like to tag a new resource?");
 	});
 });
+
+
+/* - find no content loads
+$('#iframe').load(function (e) {
+	var iframe = $("#iframe")[0];
+	console.log(e);
+	console.log(iframe);
+
+	if ( iframe.innerHTML() ) {
+		// get and check the Title (and H tags if you want)
+		var ifTitle = iframe.contentDocument.title;
+		if ( ifTitle.indexOf("404")>=0 ) {
+			// we have a winner! probably a 404 page!
+		}
+	} else {
+		// didn't load
+	}
+});
+*/
